cab2f7681dcefd080153b7bd33f8ae26bb0ba0a8
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-02-06 15:37:27 +0500

    more likes

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -14,11 +14,11 @@ public class PostService {
 
     {
         posts = new ArrayList<>();
-        Post post1 = new Post("Летучие мыши стали активнее!", new Date(999-1900, Calendar.JANUARY, 2));
-        Post post2 = new Post("Крысы съели ужин слепого человека!", new Date(567-1900, Calendar.MAY, 8));
-        Post post3 = new Post("Цветы расцвели на центральной клумбе города!", new Date(2024-1900, Calendar.FEBRUARY, 30));
+        Post post1 = new Post((long)posts.size(), "Летучие мыши стали активнее!", new Date(999-1900, Calendar.JANUARY, 2));
         posts.add(post1);
+        Post post2 = new Post((long)posts.size(), "Крысы съели ужин слепого человека!", new Date(567-1900, Calendar.MAY, 8));
         posts.add(post2);
+        Post post3 = new Post((long)posts.size(),"Цветы расцвели на центральной клумбе города!", new Date(2024-1900, Calendar.FEBRUARY, 30));
         posts.add(post3);
     }
 
@@ -27,6 +27,6 @@ public class PostService {
     }
 
     public void create(String text) {
-        posts.add(new Post(text, new Date()));
+        posts.add(new Post((long)posts.size(), text, new Date()));
     }
 }
