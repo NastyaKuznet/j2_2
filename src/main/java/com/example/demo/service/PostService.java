2995eb77e51967a14cd54c1727ce4b7ec8ec08f7
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-02-06 13:48:16 +0500

    Bootstrap begin

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,15 +4,17 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
 
     public List<Post> listAllPosts(){
-        Post post1 = new Post("Летучие мыши стали активнее!");
-        Post post2 = new Post("Крысы съели ужин слепого человека!");
-        Post post3 = new Post("Цветы расцвели на центральной клумбе города!");
+        Post post1 = new Post("Летучие мыши стали активнее!", new Date(999-1900, Calendar.JANUARY, 2));
+        Post post2 = new Post("Крысы съели ужин слепого человека!", new Date(567-1900, Calendar.MAY, 8));
+        Post post3 = new Post("Цветы расцвели на центральной клумбе города!", new Date(2024-1900, Calendar.FEBRUARY, 30));
 
         List<Post> posts = new ArrayList<Post>();
         posts.add(post1);
